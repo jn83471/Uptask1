12ea17eebf9023f431e8bd36e5036ae016c63326
jn83471@gmail.com <jn83471@gmail.com>
2025-08-07 23:06:05 -0700

    Add Show Proyects

--- frontend/src/api/ProjectApi.ts
+++ frontend/src/api/ProjectApi.ts
@@ -1,6 +1,6 @@
 import { isAxiosError } from "axios"
 import api from "../lib/axios"
-import type { ProjectFormData } from "../types"
+import { DashboarProjectShecma, type ProjectFormData } from "../types"
 
 export const CreateProject = async (formData: ProjectFormData) => {
     try {
@@ -8,6 +8,25 @@ export const CreateProject = async (formData: ProjectFormData) => {
         return data
     }
     catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response?.data.error)
+        }
+        throw new Error("Desde error")
+    }
+}
+
+export const GetProjects = async () => {
+    try {
+        const { data } = await api("/proyects")
+        console.log(data)
+        const response=await DashboarProjectShecma.safeParse(data)
+        if(response.success){
+            return response.data
+        }
+        
+    }
+    catch (error) {
+        console.log(error)
         if (isAxiosError(error) && error.response) {
             throw new Error(error.response?.data.error)
         }
--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -3,11 +3,20 @@ import {z} from 'zod'
 /* projects */
 
 export const projectSchema=z.object({
-    _id: z.string,
-    projectName: z.string,
-    clientName: z.string,
-    description: z.string,
+    _id: z.string(),
+    projectName: z.string(),
+    clientName: z.string(),
+    description: z.string(),
 })
+
+export const DashboarProjectShecma=z.array(
+    projectSchema.pick({
+        _id:true,
+        projectName:true,
+        clientName:true,
+        description:true
+    })
+)
 
 export type Project=z.infer<typeof projectSchema>
 export type ProjectFormData=Pick<Project,'clientName' | 'projectName' | 'description'>
